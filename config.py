51cf327c03c3942de96c57447f045b13ea02068e
karanchauhan72 <kc946531@gmail.com>
2026-09-12 02:49:54 +0530

    Update bot username and name in config

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # ===============================================
 
-BOT_USERNAME = getenv("BOT_USERNAME", "Meher_music_bot")
-BOT_NAME = getenv("BOT_NAME", "Meher music")
+BOT_USERNAME = getenv("BOT_USERNAME", "@Meher_music_bot")
+BOT_NAME = getenv("BOT_NAME", "˹ᴍᴇʜᴇʀ ✘ ᴍᴜꜱɪᴄ˼™")
 
 # ===============================================
 
